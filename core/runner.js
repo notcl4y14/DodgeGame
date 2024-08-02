2d9bae0b16bc5db9d8de5f7a023b908a1fd9c78e
notcl4y14 <notcl4y@gmail.com>
2024-08-02 21:23:43 +0300

    Add drawFPS()

--- core/runner.js
+++ core/runner.js
@@ -1,11 +1,17 @@
+import Display from "../front/display.js";
+
 export default class Runner {
 
 	ticks;
+	lastTicks;
+	delta;
+
 	update;
 	draw;
 
 	constructor () {
 		this.ticks = 0;
+		this.lastTicks = performance.now();
 	}
 
 	// Run
@@ -17,10 +23,24 @@ export default class Runner {
 	loop () {
 		this.ticks += 1;
 
+		this.delta = (performance.now() - this.lastTicks);
+
 		this.update();
 		this.draw();
 
+		this.lastTicks = performance.now();
+
+		this.drawFPS();
+
 		requestAnimationFrame( () => this.loop.call(this) );
+		// setTimeout( () => this.loop.call(this), this.delta / 60 );
+	}
+
+	// Draw Functions
+	drawFPS () {
+		const fps = Math.floor(1000 / this.delta);
+		Display.context.fillStyle = "#000000";
+		Display.context.fillText(`FPS: ${fps}`, 0, 10);
 	}
 
 }
