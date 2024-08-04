4aacd6ffb4cb353cb3887133e3e06ae5d5ff66c0
notcl4y14 <notcl4y@gmail.com>
2024-08-04 11:59:20 +0300

    Use entity properties and methods in HurtBox

--- content/entities/hurtbox.js
+++ content/entities/hurtbox.js
@@ -2,8 +2,6 @@ import Box from "../../core/box.js";
 
 export default class HurtBox extends Box {
 
-	velocity;
-
 	constructor (x, y, width, height) {
 		super();
 		this.setPosition(x, y);
@@ -12,33 +10,9 @@ export default class HurtBox extends Box {
 		this.color = "#ff0000";
 	}
 
-	setVelocity (x, y) {
-		this.velocity = [x, y];
-	}
-
-	applyVelocity () {
-		this.position[0] += this.velocity[0];
-		this.position[1] += this.velocity[1];
-	}
-
 	update () {
 		this.applyVelocity();
-
-		if (this.x < 0) {
-			this.position[0] = 0;
-			this.velocity[0] *= -1;
-		} else if (this.x > world.width - this.width) {
-			this.position[0] = world.width - this.width;
-			this.velocity[0] *= -1;
-		}
-
-		if (this.y < 0) {
-			this.position[1] = 0;
-			this.velocity[1] *= -1;
-		} else if (this.y > world.height - this.height) {
-			this.position[1] = world.height - this.height;
-			this.velocity[1] *= -1;
-		}
+		this.bounceOffWorld();
 	}
 	
 }
